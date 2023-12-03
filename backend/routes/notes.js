d327daad77340a0ba355a4be6bf8aa6a9ea16521
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-03 15:33:40 +0530

    Added notes route code

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const { getNotes, getNote, postNote, deleteNote, updateNote } = require('../controllers/notesController');
 
+router.delete('/:id', deleteNote);
+router.patch('/:id', updateNote)
+router.post('/', postNote);
+router.get('/:id', getNote);
+router.get('/', getNotes);
 
 module.exports = router;
