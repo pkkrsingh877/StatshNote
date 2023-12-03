428d6d8c160544e275e7041e9b79aa99a950637e
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-03 09:40:28 +0530

    Added signup and login page to auth routes and created navbar

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="">
+        <h1 class="h-1 p-3">Notes App</h1>
+        <ul class="flex flex-col lg:flex-row">
+            <li class="p-3">
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li class="p-3">
+                <NuxtLink to="/auth/login">Login</NuxtLink>
+            </li>
+            <li class="p-3">
+                <NuxtLink to="/about">About</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+@import url('https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css');
+</style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Login Page</h1>
+        <form action="">
+            <div class="field">
+                <label class="label">Username</label>
+                <div class="control has-icons-left has-icons-right">
+                    <input class="input" type="text" placeholder="username" value="">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-check"></i>
+                    </span>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Password</label>
+                <p class="control has-icons-left">
+                    <input class="input" type="password" placeholder="password">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                </p>
+            </div>
+            <div class="field">
+                <p class="control">
+                    <button class="button is-success">
+                        Login
+                    </button>
+                </p>
+            </div>
+        </form>
+        <NuxtLink to="/auth/signup">Don't have an account yet?</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
