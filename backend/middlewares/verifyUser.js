450b69c63f797de4776df8f7eb43a0d8d6bbfd70
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-03 20:44:53 +0530

    Middleware able to verify user, now to save user id to each note

--- backend/middlewares/verifyUser.js
+++ backend/middlewares/verifyUser.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 const verifyUser = (req, res, next) => {
-  const token = req.cookies.jwt; // Assuming you're storing the token in a cookie
-  console.log(token)
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized: No token provided' });
-  }
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Replace with your actual secret key
-    req.user = decoded; // Attach the user information to the request
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Unauthorized: Invalid token' });
