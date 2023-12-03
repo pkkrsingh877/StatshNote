cb8c9ce04d95c7fb8bcbeaf2bdcaa8b19c768818
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-03 15:29:53 +0530

    auth route update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { getSignup, postSignup, getLogin, postLogin } = require('../controllers/authController');
 
-
+router.post('/signup', postSignup)
+router.get('/signup', getSignup);
+router.post('/login', postLogin);
+router.get('/login', getLogin);
 
 module.exports = router;
